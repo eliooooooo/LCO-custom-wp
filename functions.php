a97d3efb3e2c98afa6a073139c91fe29b19d55a6
Eliott <eliott.burkle@gmail.com>
2024-05-30 13:20:29 +0200

    Feat(format): add button format to WYSIWYG editor

--- functions.php
+++ functions.php
@@ -201,3 +201,25 @@ function mce_css( $mce_css ) {
 	return $mce_css;
 }
 add_filter( 'mce_css', 'mce_css' );
+
+
+function my_mce_buttons_2($buttons) {
+	array_unshift($buttons, 'styleselect');
+	return $buttons;
+}
+add_filter('mce_buttons_2', 'my_mce_buttons_2');
+
+
+function my_mce_before_init_insert_formats($init_array) {
+	$style_formats = array(
+		array(
+			'title' => 'Bouton',
+			'block' => 'span',
+			'classes' => 'btn blue',
+			'wrapper' => true,
+		),
+	);
+	$init_array['style_formats'] = json_encode($style_formats);
+	return $init_array;
+}
+add_filter('tiny_mce_before_init', 'my_mce_before_init_insert_formats');
