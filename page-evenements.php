7685a438810be2f8be342c8c2af1e7e6853dd6af
eliooooooo <eliott.burkle@gmail.com>
2024-05-31 14:17:29 +0200

    Feat(events): add thumbnail image

--- page-evenements.php
+++ page-evenements.php
@@ -62,62 +62,69 @@ get_header(); ?>
 					<?php } ?>
                 </div>
                 <div class="infos-pratiques w-full md:w-5/12 lg:w-4/12">
-					<div class="w-full border border-solid border-gray-400 rounded-md py-8 px-4">
-						<?php if($lieu){ ?>
-							<div class="flex flex-row gap-4 items-center mb-4">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-									<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
-								</svg>
-								<p><?php echo $lieu; ?></p>
+					<div class="relative w-full border border-solid border-gray-400 rounded-md overflow-hidden">
+						<?php if(has_post_thumbnail()){ ?>
+							<div class="w-full h-auto mx-auto">
+								<?php the_post_thumbnail('thumbnail', array('class' => 'w-full')); ?>
 							</div>
 						<?php } ?>
-						<?php if($dates){ ?>
-							<div class="flex flex-row gap-4 items-center mb-4">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-calendar-week" viewBox="0 0 16 16">
-									<path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
-									<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
-								</svg>
-								<p><?php echo $dates; ?></p>
-							</div>
-						<?php } ?>
-						<?php if($nom_intervenant || $tel_intervenant || $email_intervenant){ ?>
-							<div class="flex flex-row gap-4 items-start mb-4">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-									<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-								</svg>
-								<div class="flex flex-col gap-2" >
-									<?php if($nom_intervenant){ ?>
-										<span><?php echo $nom_intervenant; ?></span>
+						<div class="py-8 px-4">
+							<?php if($lieu){ ?>
+								<div class="flex flex-row gap-4 items-center mb-4">
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+										<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+									</svg>
+									<p><?php echo $lieu; ?></p>
+								</div>
+							<?php } ?>
+							<?php if($dates){ ?>
+								<div class="flex flex-row gap-4 items-center mb-4">
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-calendar-week" viewBox="0 0 16 16">
+										<path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+										<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+									</svg>
+									<p><?php echo $dates; ?></p>
+								</div>
+							<?php } ?>
+							<?php if($nom_intervenant || $tel_intervenant || $email_intervenant){ ?>
+								<div class="flex flex-row gap-4 items-start mb-4">
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+										<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+									</svg>
+									<div class="flex flex-col gap-2" >
+										<?php if($nom_intervenant){ ?>
+											<span><?php echo $nom_intervenant; ?></span>
+										<?php } ?>
+										<?php if($tel_intervenant){ ?>
+											<span><?php echo $tel_intervenant; ?></span>
+										<?php } ?>
+										<?php if($email_intervenant){ ?>
+											<span><?php echo $email_intervenant; ?></span>
+										<?php } ?>
+									</div>
+								</div>
+							<?php } ?>
+							<?php if($tarif){ ?>
+								<div class="flex flex-row gap-4 items-center">
+									<h4 class="font-bold">Tarif</h4>
+									<p><?php echo $tarif; ?></p>
+								</div>
+							<?php } ?>
+							<?php if($nom_coordo || $tel_coordo || $email_coordo){ ?>
+								<div class="flex flex-col items-center justify-center text-center mt-8 text-gray-400">
+									<span><u>Coordinateur LCO :</u></span>
+									<?php if($nom_coordo){ ?>
+										<span><?php echo $nom_coordo; ?></span>
 									<?php } ?>
-									<?php if($tel_intervenant){ ?>
-										<span><?php echo $tel_intervenant; ?></span>
+									<?php if($email_coordo){ ?>
+										<span><?php echo $email_coordo; ?></span>
 									<?php } ?>
-									<?php if($email_intervenant){ ?>
-										<span><?php echo $email_intervenant; ?></span>
+									<?php if($tel_coordo){ ?>
+										<span><?php echo $tel_coordo; ?></span>
 									<?php } ?>
 								</div>
-							</div>
-						<?php } ?>
-						<?php if($tarif){ ?>
-							<div class="flex flex-row gap-4 items-center">
-								<h4 class="font-bold">Tarif</h4>
-								<p><?php echo $tarif; ?></p>
-							</div>
-						<?php } ?>
-						<?php if($nom_coordo || $tel_coordo || $email_coordo){ ?>
-							<div class="flex flex-col items-center justify-center text-center mt-8 text-gray-400">
-								<span><u>Coordinateur LCO :</u></span>
-								<?php if($nom_coordo){ ?>
-									<span><?php echo $nom_coordo; ?></span>
-								<?php } ?>
-								<?php if($email_coordo){ ?>
-									<span><?php echo $email_coordo; ?></span>
-								<?php } ?>
-								<?php if($tel_coordo){ ?>
-									<span><?php echo $tel_coordo; ?></span>
-								<?php } ?>
-							</div>
-						<?php } ?>
+							<?php } ?>
+						</div>
 					</div>
                 </div>
             </div>
