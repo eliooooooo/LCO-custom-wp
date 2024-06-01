65b5b363cbc855800a17ece17ce1200134647ec1
eliooooooo <eliott.burkle@gmail.com>
2024-06-01 11:39:13 +0200

    Feat(frontpage): update frontpage

--- frontpage.php
+++ frontpage.php
@@ -18,7 +18,7 @@ $image_5050 = get_field('bloc_5050')['image'];
 <?php get_header(); ?>
 
 <?php if($banner){ ?>
-    <div class="banner h-[250px] lg:h-[350px] bg-cover bg-center bg-no-repeat" style="background-image:url(<?php echo $banner['url']; ?>)">
+    <div class="banner h-[220px] bg-cover bg-center bg-no-repeat" style="background-image:url(<?php echo $banner['url']; ?>)">
         <div class="relative w-full text-center h-full">
             <h1 class="absolute flex flex-col gap-2 lg:gap-4 items-center text-center font-bold text-white drop-shadow-xl bottom-8 w-full max-w-full">
                 <span class="text-2xl sm:text-2xl lg:text-4xl"><?php echo $title1; ?></span>
@@ -30,30 +30,17 @@ $image_5050 = get_field('bloc_5050')['image'];
 
 <div class="container mx-auto my-8">
 
-    <?php if ($titre_5050 || $texte_5050 || $image_5050){ ?>
+    <?php if ($titre_5050 || $texte_5050){ ?>
         <?php echo generate_title($titre_5050, 'h2') ?>
         <div class="flex flex-col lg:flex-row gap-4 lg:gap-8">
             <div class="flex flex-col gap-4 lg:w-1/2">
                 <?php echo $texte_5050; ?>
             </div>
             <div class="lg:w-1/2">
-                <img src="<?php echo $image_5050['url']; ?>" alt="<?php echo $image_5050['alt']; ?>" class="w-full h-auto rounded-md">
+                <!-- Ajouter les actualités ici ! -->
             </div>
         </div>
     <?php } ?>
-    
-    <?php if ( have_posts() ) : ?>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			?>
-
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-
-		<?php endwhile; ?>
-
-	<?php endif; ?>
 
 </div>
 
