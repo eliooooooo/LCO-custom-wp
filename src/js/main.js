af905f4d9244321c0c83da7d4a56be7db29438bb
eliooooooo <eliott.burkle@gmail.com>
2024-06-02 15:37:36 +0200

    Feat(alpine): alpinejs initialisation

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 import './../css/style.css';
 import './swiper.js';
 
-console.log('Hello world !'); 
+// console.log('Hello world !'); 
+
+import Alpine from 'alpinejs'
+document.addEventListener('DOMContentLoaded', function() {
+    window.Alpine = Alpine
+    
+    Alpine.start()
+});
