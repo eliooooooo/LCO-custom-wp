c1fff9de2a9dd8e98bd64370eeacec6afcffd35a
eliooooooo <eliott.burkle@gmail.com>
2024-06-04 16:00:30 +0200

    Feat: container integration

--- archive-evenements.php
+++ archive-evenements.php
@@ -22,32 +22,45 @@ rewind_posts();
 if (have_posts()) :
 ?>
   <div class="container mx-auto">
-    <div class="mt-10 mb-10 w-full flex flex-row flex-wrap gap-4 sm:gap-6 md:gap-8 justify-around items-center">
-      <?php while (have_posts()) { 
-        the_post();
-        $categories = get_the_category();
-        $infos_pratiques = get_field('infos_pratiques', $activite_id);
-        $dates_cards = $infos_pratiques['date_cards'];
-        $ligne1 = $dates_cards['ligne_1'];
-        $ligne2 = $dates_cards['ligne_2'] ?>
-        <div class="relative w-full sm:w-[45%] h-[250px]">
-          <div class="absolute -top-4 -left-4 bg-white text-lco_blue-500 p-3 shadow-md z-10 flex flex-col items-center justify-center gap-2">
-            <span class="font-bold text-center"><?php echo $ligne1; ?></span>
-            <span class="font-bold text-center"><?php echo $ligne2; ?></span>
-          </div>
-          <a href="<?php echo get_the_permalink(); ?>" class="block zoomable-container bg-gray-200 w-full h-full relative overflow-hidden transform scale-100" >
-            <div class="w-full h-full bg-no-repeat bg-cover bg-center zoomable" <?php if(has_post_thumbnail()) {?> style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)" <?php } ?> ></div>
-            <div class="absolute bottom-0 w-full">
-              <span class="absolute left-4 bottom-[calc(100%-20px)] flex flex-row gap-4 bg-white text-lco_blue-500 font-bold text-lg px-4 py-1.5">
-                <?php foreach($categories as $category){ ?>
-                  <span><?php echo $category->name; ?></span>
+    <div class="flex flex-col lg:flex-row gap-10 items-center justify-between">
+      <div class="w-full lg:w-4/12 xl:w-3/12 h-52 bg-gray-200">
+
+      </div>
+      <div class="mt-10 mb-10 w-full lg:w-8/12 xl:w-9/12 flex flex-row flex-wrap gap-4 sm:gap-6 md:gap-8 justify-around items-center px-2 sm:px-0">
+        <?php while (have_posts()) { 
+          the_post();
+          $categories = get_the_category();
+          $infos_pratiques = get_field('infos_pratiques', $activite_id);
+          $dates_cards = $infos_pratiques['date_cards'];
+          $ligne1 = $dates_cards['ligne_1'];
+          $ligne2 = $dates_cards['ligne_2'] ?>
+          <div class="relative w-full sm:w-[45%] h-[250px]">
+            <?php if($dates_cards){ ?>
+              <div class="absolute -top-4 -left-4 bg-white text-lco_blue-500 p-3 shadow-md z-10 flex flex-col items-center justify-center gap-2">
+                <?php if($ligne1){ ?>
+                  <span class="font-bold text-center"><?php echo $ligne1; ?></span>
                 <?php } ?>
-              </span>
-              <h3 class="bg-lco_blue-500 text-white px-4 py-6 "><?php echo get_the_title(); ?></h3>
-            </div>
-          </a>
-        </div>
-      <?php } ?>
+                <?php if($ligne2){ ?>
+                  <span class="font-bold text-center"><?php echo $ligne2; ?></span>
+                <?php } ?>
+              </div>
+            <?php } ?>
+            <a href="<?php echo get_the_permalink(); ?>" class="block zoomable-container bg-gray-200 w-full h-full relative overflow-hidden transform scale-100" >
+              <div class="w-full h-full bg-no-repeat bg-cover bg-center zoomable" <?php if(has_post_thumbnail()) {?> style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)" <?php } ?> ></div>
+              <div class="absolute bottom-0 w-full">
+                <?php if($categories){ ?>
+                  <span class="absolute left-4 bottom-[calc(100%-20px)] flex flex-row gap-4 bg-white text-lco_blue-500 font-bold text-lg px-4 py-1.5">
+                    <?php foreach($categories as $category){ ?>
+                      <span><?php echo $category->name; ?></span>
+                    <?php } ?>
+                  </span>
+                <?php } ?>
+                <h3 class="bg-lco_blue-500 text-white px-4 py-6 "><?php echo get_the_title(); ?></h3>
+              </div>
+            </a>
+          </div>
+        <?php } ?>
+      </div>
     </div>
   </div>
 <?php endif; ?>
