71d7c177ef0ac9e7d45543bcd003e2a21d6e97ac
eliooooooo <eliott.burkle@gmail.com>
2024-06-06 15:14:17 +0200

    Feat: update footer + add socket

--- footer.php
+++ footer.php
@@ -6,27 +6,39 @@
 </div>
 
 <footer class="site-footer py-8 bg-black text-white">	
-	<div class="container mx-auto flex flex-col sm:flex-row items-center sm:items-start justify-between gap-4 sm:gap-8 md:gap-12 py-4">
+	<div class="container mx-auto flex flex-col sm:flex-row items-center sm:items-start justify-around gap-4 sm:gap-8 md:gap-12 py-4 mb-4">
 		<ul class="flex flex-col items-center sm:items-start text-center sm:text-left">
 			<li class="font-bold"><?php echo get_bloginfo( 'name' ); ?></li>
 			<li>Mail : secretariat.lco@gmail.com</li>
 		</ul>
-		<div>
+		<div class="menu">
 			<?php
 				wp_nav_menu(
 					array(
 						'theme_location' => 'footer',
 						'container'      => false,
 						'menu_class'     => 'flex flex-col items-start gap-4',
-						'li_class'       => ''
+						'li_class'       => 'text-center'
 					)
 				);
 			?>
 		</div>
 	</div>
-	<!-- <div class="text-center mt-4 text-gray-400">
-		&copy; <?php // echo date_i18n( 'Y' );?> - <?php // echo get_bloginfo( 'name' );?>
-	</div> -->
+	<div class="socket flex flex-col sm:flex-row items-center justify-center gap-6 menu text-gray-400">
+		<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'socket',
+					'container'      => false,
+					'menu_class'     => 'flex flex-row items-center gap-4',
+					'li_class'       => 'text-center'
+				)
+			);
+		?>
+		<div class="text-center">
+			&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?>
+		</div>
+	</div>
 </footer>
 
 </div>
