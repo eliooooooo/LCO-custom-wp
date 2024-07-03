97b64cc499b70da1d13e2aada8061b05e642f981
Eliott <eliott.burkle@gmail.com>
2024-07-03 18:27:42 +0200

    Style: Remove site title on frontpage 🤡

--- frontpage.php
+++ frontpage.php
@@ -25,8 +25,6 @@ $query = new WP_Query($args);
 $frontpage_id = get_option( 'page_on_front' );
 
 $banner = get_field('header')['image_header'];
-$title1 = get_field('header')['titre_header_1'];
-$title2 = get_field('header')['titre_header_2'];
 
 $titre_5050 = get_field('bloc_5050')['titre'];
 $texte_5050 = get_field('bloc_5050')['texte'];
@@ -38,12 +36,6 @@ $image_alternative = get_field('bloc_5050')['image_alternative'];
 
 <?php if($banner){ ?>
     <div class="banner h-[220px] bg-cover bg-center bg-no-repeat" style="background-image:url(<?php echo $banner['url']; ?>); background-position-y: 25%;">
-        <div class="relative w-full text-center h-full">
-            <h1 class="absolute flex flex-col gap-2 lg:gap-4 items-center text-center font-bold text-white drop-shadow-xl bottom-8 w-full max-w-full">
-                <span class="text-2xl sm:text-2xl lg:text-4xl"><?php echo $title1; ?></span>
-                <span class="text-3xl sm:text-4xl lg:text-6xl"><?php echo $title2; ?></span>
-            </h1>
-        </div>
     </div>
 <?php } ?>
 
